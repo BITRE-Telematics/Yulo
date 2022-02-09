e5ffe7a319ea6746fead3510e90abb19e10af5a9
Richard <novo.mestro@gmail.com@gmail.com>
2022-02-10 10:34:14 +1100

    refactored stop clustering.py

--- stopclustering/stop_cluster.py
+++ stopclustering/stop_cluster.py
@@ -26,14 +26,15 @@ def stop_query(tx, YEAR, MONTH):
 	return df
 
 
-def pull_stops(creds, year, month):
+def pull_stops(creds, year, month, del_stop_edges = False):
 	g = neo4j.GraphDatabase.driver("bolt://%s" % creds['ipport'], auth = (creds['username'], creds['password']))
 	with g.session() as session:
 		ps = session.read_transaction(stop_query, YEAR = year, MONTH = month)
 		#in case clustering is being rerun with new data
 		session.run("MATCH(c:Cluster) WHERE c.month = $month AND c.year = $year DETACH DELETE c", month = month, year = year)
-		session.run("MATCH(s:Stop)-[u:USED]->(l:Location) WHERE s.start_timedt.month = $month AND s.start_timedt.year = $year DELETE u", month = month, year = year)
-		session.run("MATCH(s:Stop)-[a:AT]->(Address) WHERE s.start_timedt.month = $month AND s.start_timedt.year =  $year DELETE a", month = month, year = year)
+		if del_stop_edges:
+			session.run("MATCH(s:Stop)-[u:USED]->(l:Location) WHERE s.start_timedt.month = $month AND s.start_timedt.year = $year DELETE u", month = month, year = year)
+			session.run("MATCH(s:Stop)-[a:AT]->(Address) WHERE s.start_timedt.month = $month AND s.start_timedt.year =  $year DELETE a", month = month, year = year)
 	return ps
 
 
@@ -46,17 +47,48 @@ def cluster_stops(df, year, month, eps = 1000, min_stops = 10):
 
 ## anonymity can also be checked in database
 
-def write_cluster(tup, creds, year, month, n_firms = 2, n_veh = 3):
+def make_row(tup, n_firms, n_veh, year, month):
 	anon = len(set(tup[1].firm)) >= n_firms and len(set(tup[1].vehicle)) >= n_veh
-	g = neo4j.GraphDatabase.driver("bolt://%s" % creds['ipport'], auth = (creds['username'], creds['password']))
-	row =  {'cluster': tup[0], 'anon': anon, 'lat': tup[1].lat.mean(), 'lon': tup[1].lon.mean(), 'year': year, 'month': month}
-	with g.session() as session:
-		session.run("CREATE (c:Cluster{id: $cluster}) SET c.anon = toBoolean($anon), c.lat = $lat, c.lon = $lon, c.year = $year, c.month = $month",
-		 row)
-		stop_ids = tup[1].apply(lambda x: session.run("MATCH (c:Cluster{id:$cluster}) MATCH (s:Stop{id: $stop}) CREATE (s)-[:PART_OF]->(c)",
-		 cluster = tup[0], stop = x.id), axis = 1)
-		#print([s.value() for s in stop_ids])
+	row =  {
+		'cluster': tup[0],
+		'anon': anon,
+		'lat': tup[1].lat.mean(),
+		'lon': tup[1].lon.mean(),
+		'year': year,
+		'month': month,
+		"stops" : tup[1].id.tolist()
+		}
 	return row
+
+
+def make_clust_dict(stops, n_firms, n_veh, year, month):
+	clusters = [make_row(c, n_firms, n_veh, year, month) for c in stops.groupby('cluster')]
+	clusters = dict(CLUSTERS = clusters)
+	return clusters
+
+def write_cluster(clusters, creds, year, month, n_firms = 2, n_veh = 3):
+	
+	g = neo4j.GraphDatabase.driver("bolt://%s" % creds['ipport'], auth = (creds['username'], creds['password']))
+	
+	query = "UNWIND $CLUSTERS as clust\
+       CREATE (c:Cluster{id: clust.cluster}) SET c.anon = toBoolean(clust.anon), c.lat = clust.lat, c.lon = clust.lon, c.year = clust.year, c.month = clust.month\
+       	FOREACH (ignoreMe IN CASE WHEN clust.loc_id <> '' THEN [1] ELSE [] END |\
+        	MERGE (loc_id:Location{id: clust.loc_id})\
+        	MERGE (c)-[:USED]->(loc_id)\
+        )\
+        FOREACH (ignoreMe IN CASE WHEN clust.addr <> '' THEN [1] ELSE [] END |\
+        	MERGE (addr:Address{id: clust.addr})\
+			MERGE (c)-[:AT]->(addr)\
+       )\
+       \
+       WITH c, clust\
+       UNWIND clust.stops as s_id\
+       MATCH (s:Stop{id: s_id})\
+       MERGE (s)-[:PART_OF]->(c)\
+      "
+	with g.session() as session:
+		session.run(query, clusters)
+	return clusters
 
 
 ##Consider different parameteres for loading zones and rest areas. This can be applied afterwards since lz threhold will likely always be less than ra threshold
@@ -91,15 +123,6 @@ def loc_query(tx):
 
 
 
-def write_lz_rest(row, creds, node_type):
-	db = 'bolt://%s' % creds['ipport']
-	col = 'cluster' if node_type == 'Cluster' else 'id'
-	query = 'MATCH(c:%s{id: {%s}})\
-					 MERGE(l:Location{id:$loc_id})\
-					 CREATE(c)-[:USED]->(l)' % (node_type, col)
-	g = neo4j.GraphDatabase.driver(db, auth = (creds['username'], creds['password']))
-	with g.session() as session:
-		session.run(query, row)
 
 ##This duplicates much of the above but I can't merge them easily because of the QLD rest area bug thing
 
@@ -121,30 +144,65 @@ def addr_query(tx):
 
 
 
-def write_addr(row, creds, node_type):
+
+def write_lz_rest(stops_loc, creds, node_type):
 	db = 'bolt://%s' % creds['ipport']
 	col = 'cluster' if node_type == 'Cluster' else 'id'
-	query = 'MATCH(c:%s{id: {%s}})\
-					 MERGE(l:Address{id:$loc_id})\
-					 CREATE(c)-[:AT]->(l)' % (node_type, col)
+	query = 		'UNWIND $STOPS as s\
+					 MERGE(c:%s{id: s.%s})\
+					 MERGE(l:Location{id:s.loc_id})\
+					 CREATE(c)-[:USED]->(l)' % (node_type, col)
+
+	stops_loc = [dict(c[1]) for c in stops_loc.iterrows()]
+	stops_loc = dict(STOPS = stops_loc)
 	g = neo4j.GraphDatabase.driver(db, auth = (creds['username'], creds['password']))
 	with g.session() as session:
-		print(row)
-		session.run(query, row)
+		session.run(query, stops_loc)
 
 
 
-def ckdnearest(gdA, gdB, bcol, max_dist = 100):   
+def write_addr(stops_addr, creds, node_type):
+	db = 'bolt://%s' % creds['ipport']
+	col = 'cluster' if node_type == 'Cluster' else 'id'
+	query =         'UNWIND $STOPS as s\
+					 MERGE(c:%s{id: s.%s})\
+					 MERGE(l:Address{id:s.loc_id})\
+					 CREATE(c)-[:AT]->(l)' % (node_type, col)
+	##I can groupby address for minor performance improvement
+	stops_addr = [dict(c[1]) for c in stops_addr.iterrows()]
+	stops_addr = dict(STOPS = stops_addr)
+	g = neo4j.GraphDatabase.driver(db, auth = (creds['username'], creds['password']))
+	with g.session() as session:
+		session.run(query, stops_addr)
+
+
+
+def ckdnearest(gdA, gdB, bcol, max_dist = 100, clust_match = False):   
     nA = np.array(list(zip(gdA.lon, gdA.lat)) )
     nB = np.array(list(zip(gdB.lon, gdB.lat)) )
     btree = cKDTree(nB)
     dist, idx = btree.query(nA, k=1, n_jobs = -1) ##this is fairly fast but can use n_jobs to parallelise. -1 is all cores
     dist = dist * 110250 ## turning euclidean distance into approxiamte metres
     df = pd.DataFrame.from_dict({'distance': dist.astype(int),
-                             bcol : gdB.loc[idx, bcol].values })
+                             bcol : gdB.loc[idx, 'loc_id'].values })
     df = pd.concat([gdA, df], axis = 1)
-    df = df[df.distance <= max_dist] ##this could be done with distance_upper_bound = 1/110250 *100 in btree.query()
+    if clust_match:
+        df[bcol] = df.apply(lambda x: x[bcol] if x.distance <= max_dist else "", axis = 1)
+    else:
+        df = df[df.distance <= max_dist] ##this could be done with distance_upper_bound = 1/110250 *100 in btree.query()
+	
     return df
+
+def match_clusters(clusters, locs, addr_locs, max_dist, max_dist_addr):
+	df = pd.DataFrame.from_records(clusters["CLUSTERS"])
+	df['loc_id'] = ckdnearest(df, locs, 'loc_id', max_dist, clust_match=True).loc_id
+	df['addr'] = ckdnearest(df, addr_locs, 'addr', max_dist_addr, clust_match = True).addr
+	lst = df.apply(lambda x: dict(x), axis = 1)
+	return {"CLUSTERS" : lst.tolist()}
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -166,9 +224,9 @@ if __name__ == "__main__":
 						help="Episilon (in metres I think)")
 	parser.add_argument("-ms", "--min_stops", type = int, default = 5,
 						help="Min cluster size")
-	parser.add_argument("-md", "--max_dist", type = float, default = 150,
+	parser.add_argument("-md", "--max_dist", type = float, default = 400,
 						help="Max distance to be associated with a site")
-	parser.add_argument("-mda", "--max_dist_addr", type = float, default = 50,
+	parser.add_argument("-mda", "--max_dist_addr", type = float, default = 200,
 						help="Max distance to be associated with an address")
 	parser.add_argument("-mis", "--match_ind_stops", type = bool, default = False,
 						help="Match individual stops to locations - should be done by yuloserver now")
@@ -185,36 +243,37 @@ if __name__ == "__main__":
 		creds = yaml.safe_load(credsfile)
 
 	print("Pulling stops for %d-%d" % (args.year, args.month))
-	stops = pull_stops(creds, args.year, args.month)
+	stops = pull_stops(creds, args.year, args.month, args.match_ind_stops)
+	addr_locs = get_addr(creds = creds)
+	locs = get_ras_lz(creds = creds)
 
 	if args.match_ind_stops:
 		print('matching %s stops to rest areas and loading zones' % len(stops.index))
-		locs = get_ras_lz(creds = creds)
+		
 		stops_loc = lz_rest_match(stops, locs = locs, max_dist = args.max_dist, creds = creds)
 		print('Writing edges between %s locations and %s stops' % (len(set(stops_loc.loc_id)), len(stops_loc.index)))
-		stops_loc = [dict(c[1]) for c in stops_loc.iterrows()]
+		
 		write_lz_rest_s =partial(write_lz_rest, creds = creds, node_type =  "Stop")
-		with Pool() as p:
-			p.map(write_lz_rest_s, stops_loc)
+		write_lz_rest_s(stops_loc)
 
 
 		print('matching %s stops to addresses' % len(stops.index))
-		addr_locs = get_addr(creds = creds)
+		
 
 		stops_addr = addr_match(stops, locs = addr_locs, max_dist = args.max_dist_addr, creds = creds)
 		print('Writing edges between %s addresses and %s stops' % (len(set(stops_addr.loc_id)), len(stops_addr.index)))
-		stops_addr = [dict(c[1]) for c in stops_addr.iterrows()]
+		
 		write_addr_s =partial(write_addr, creds = creds, node_type =  "Stop")
-		with Pool(1) as p:
-			p.map(write_addr_s, stops_addr)
+		
+		write_addr_s(stops_addr)
 
 	 
 
 	print("Clustering %s stops" % len(stops.index))
 	stops = cluster_stops(stops, args.year, args.month, eps = args.epsilon, min_stops = args.min_stops)
 	stops = stops[stops.inCluster]
-	
-	
+	clusters = make_clust_dict(stops, args.n_firms, args.n_vehicles, args.year, args.month)
+	clusters = match_clusters(clusters, locs, addr_locs, args.max_dist, args.max_dist_addr)
 	print("Writing %s stop clusters" % len(set(stops.cluster)))
 	write_cluster1 = partial(write_cluster,
 							creds = creds,
@@ -223,26 +282,8 @@ if __name__ == "__main__":
 							n_firms = args.n_firms,
 							n_veh = args.n_vehicles
 							)
-	with Pool(1) as p:
-		clusters = p.map( write_cluster1, stops.groupby('cluster'))
-
-	clusters = pd.DataFrame(clusters)
-
-	print('Matching to addresses')
-	clusters_addr = addr_match(clusters, locs = addr_locs, max_dist = args.max_dist_addr, creds = creds)
 	
-	clusters_addr = [dict(c[1]) for c in clusters_addr.iterrows()]
-	write_addr_c =partial(write_addr, creds = creds, node_type =  "Cluster")
-	with Pool() as p:
-		p.map(write_addr_c, clusters_addr)
+	clusters = write_cluster1(clusters)
 
-
-	print('Matching to rest areas and loading zones')
-	clusters = lz_rest_match(clusters, locs = locs, max_dist = args.max_dist, creds = creds)
 	
-	clusters = [dict(c[1]) for c in clusters.iterrows()]
-	write_lz_rest_c =partial(write_lz_rest, creds = creds, node_type =  "Cluster")
-	with Pool(1) as p:
-		p.map(write_lz_rest_c, clusters)
-
 
